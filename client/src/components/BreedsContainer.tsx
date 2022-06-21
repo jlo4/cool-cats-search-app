d0b21b87ec69e5a2631cdc6780d663495d64ea2d
John Love <john.love4@gmail.com>
2022-06-21 19:34:57 +0300

    Add searching by name

--- client/src/components/BreedsContainer.tsx
+++ client/src/components/BreedsContainer.tsx
@@ -1,31 +1,51 @@
 import React from 'react'
-import '../styles/BreedsContainer.scss'
+import { distance } from 'fastest-levenshtein'
+import '../styles/breedsContainer.scss'
 import { Breed } from '../../../types'
 
 interface BreedsContainerProps {
     breeds: Breed[]
     page: number
     pageSize: number
+    searchValue: string
 }
 
-const BreedItem = ({breed, key}: {
+const BreedItem = ({breed, id}: {
     breed: Breed,
-    key: string
+    id: string
 }) => {
     return (
-        <div className='breed-item' key={key}>
-            {breed.id.toString()}
+        <div className='breed-item' key={id}>
+            {breed.name.toString()}
         </div>
     )
 }
 
-export const BreedsContainer = ({ breeds, page, pageSize  }: BreedsContainerProps) => {    
+export const BreedsContainer = ({ breeds, page, pageSize, searchValue  }: BreedsContainerProps) => {    
+    const filterSearchValue = (searchValue: string, value: string): boolean => {
+        const SEARCH_DISTANCE = 5
+        const NOT_INCLUDED = -1
+        if (value.toLowerCase().indexOf(searchValue.toLowerCase()) > NOT_INCLUDED || distance(searchValue.toLowerCase(), value.toLowerCase()) < SEARCH_DISTANCE) {
+            return true
+        }
+        return false
+    }
+
+
     return (
         <div className='breeds-container'>
-            {
+            {                
+                searchValue ? breeds.map((breed) => {                    
+                    if (filterSearchValue(searchValue, breed.name.toString())) {
+                        return (
+                            <BreedItem breed={breed} key={breed.id.toString()} id={breed.id.toString()} />
+                        )
+                    }
+                })
+                :
                 breeds.slice((page - 1) * pageSize, (page * pageSize)).map((breed) => {
                     return (
-                        <BreedItem breed={breed} key={breed.id.toString()} />
+                        <BreedItem breed={breed} key={breed.id.toString()} id={breed.id.toString()} />
                     )
                 })
             }
